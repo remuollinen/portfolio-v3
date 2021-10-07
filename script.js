17163fc49bfa53dfdc0bfeb1a5e174564eee633f
Remu Ollinen <remu.ollinen@gmail.com>
2021-10-07 11:42:23 +0300

    added mobile menu

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const menuIcon = document.querySelector(".menu-icon");
 if (window.innerWidth > 320) {
 	window.onscroll = function () {
 		if (window.scrollY >= 600) {
-			navbar.style.backgroundColor = "#0F1B2E";
+			navbar.style.backgroundColor = "var(--dark)";
 			logo.setAttribute("src", "./assets/vectors/logo_light.svg");
 			links.forEach((link) => {
 				link.classList.add("active");
@@ -26,12 +26,12 @@ if (window.innerWidth > 320) {
 } else if (window.innerWidth <= 320) {
 	window.onscroll = function () {
 		if (window.scrollY >= 400) {
-			navbar.style.backgroundColor = "#0F1B2E";
+			navbar.style.backgroundColor = "var(--dark)";
 			logo.setAttribute("src", "./assets/vectors/logo_light.svg");
 			links.forEach((link) => {
 				link.classList.add("active");
 			});
-			menuIcon.style.color = "#eff7fb";
+			menuIcon.style.color = "var(--light)";
 		} else if (window.scrollY < 400) {
 			navbar.style.backgroundColor = "transparent";
 			logo.setAttribute("src", "./assets/vectors/logo_dark.svg");
@@ -41,4 +41,33 @@ if (window.innerWidth > 320) {
 			menuIcon.style.color = "unset";
 		}
 	};
+}
+
+menuIcon.addEventListener("click", toggleMenu);
+links.forEach((link) => link.addEventListener("click", toggleMenu));
+
+function toggleMenu() {
+	linkWrapper.classList.toggle("show");
+	if (linkWrapper.classList.contains("show")) {
+		setTimeout(darkNav, 270);
+	} else {
+		// transparentNav();
+		setTimeout(transparentNav, 270);
+	}
+}
+
+// These function change the styling of the navbar depending whether the mobile menu is open or not
+
+function darkNav() {
+	navbar.style.backgroundColor = "var(--dark)";
+	logo.setAttribute("src", "./assets/vectors/logo_light.svg");
+	menuIcon.style.color = "var(--light)";
+}
+
+function transparentNav() {
+	if (window.scrollY < 400) {
+		navbar.style.backgroundColor = "transparent";
+		logo.setAttribute("src", "./assets/vectors/logo_dark.svg");
+		menuIcon.style.color = "var(--dark)";
+	}
 }
